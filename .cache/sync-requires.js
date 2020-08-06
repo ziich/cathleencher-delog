82006a0b19b19c497afa4fea76b2948fee9ce785
ziich <chenchristophert@gmail.com>
2020-08-06 17:58:09 +0800

    fix image cover issue, add dark mode text shadow

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/ziich/projects/cathleencher-delog/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/ziich/projects/cathleencher-delog/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/ziich/projects/cathleencher-delog/src/pages/404.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/home/ziich/projects/cathleencher-delog/src/pages/contact.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/ziich/projects/cathleencher-delog/src/pages/index.js"))),
